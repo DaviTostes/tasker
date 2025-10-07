2582954c3eeee47ee2efca4ea0f02b623be8bb8b
Davi Tostes <davisiqueira591@gmail.com>
2025-10-07 00:05:00 -0300

    refactor: criando map de keys:

--- internal/tui/update.go
+++ internal/tui/update.go
@@ -2,15 +2,11 @@ package tui
 
 import (
 	"fmt"
-	"log"
 
-	"github.com/atotto/clipboard"
 	"github.com/charmbracelet/bubbles/spinner"
 	"github.com/charmbracelet/bubbles/viewport"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/glamour"
 	"github.com/charmbracelet/lipgloss"
-	"github.com/kujtimiihoxha/vimtea"
 )
 
 func setViewportError(viewport viewport.Model, err error) {
@@ -21,93 +17,38 @@ func setViewportError(viewport viewport.Model, err error) {
 	)
 }
 
+type batch struct {
+	tiCmd     tea.Cmd
+	vpCmd     tea.Cmd
+	spCmd     tea.Cmd
+	editorCmd tea.Cmd
+	cmds      []tea.Cmd
+}
+
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	var (
-		tiCmd     tea.Cmd
-		vpCmd     tea.Cmd
-		spCmd     tea.Cmd
-		editorCmd tea.Cmd
-		cmds      []tea.Cmd
-	)
+	b := batch{}
 
 	if m.isEditing {
-		m.editor, editorCmd = m.editor.Update(msg)
-		cmds = append(cmds, editorCmd)
+		m.editor, b.editorCmd = m.editor.Update(msg)
+		b.cmds = append(b.cmds, b.editorCmd)
 	} else {
-		m.textarea, tiCmd = m.textarea.Update(msg)
-		m.viewport, vpCmd = m.viewport.Update(msg)
-		cmds = append(cmds, tiCmd, vpCmd)
+		m.textarea, b.tiCmd = m.textarea.Update(msg)
+		m.viewport, b.vpCmd = m.viewport.Update(msg)
+		b.cmds = append(b.cmds, b.tiCmd, b.vpCmd)
 	}
 
 	if m.isLoading {
-		m.spinner, spCmd = m.spinner.Update(msg)
-		cmds = append(cmds, spCmd)
+		m.spinner, b.spCmd = m.spinner.Update(msg)
+		b.cmds = append(b.cmds, b.spCmd)
 	}
+
+	keyMap := GetKeyMap(m, b)
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
-		switch msg.Type {
-		case tea.KeyCtrlC:
-			return m, tea.Quit
-
-		case tea.KeyEnter:
-			if !m.isEditing && !m.isLoading {
-				userMessage := m.textarea.Value()
-				if userMessage == "" {
-					return m, nil
-				}
-
-				m.isLoading = true
-				m.textarea.Reset()
-				m.viewport.GotoBottom()
-
-				return m, tea.Batch(performGeneration(userMessage), m.spinner.Tick)
-			}
-
-		case tea.KeyCtrlY:
-			if !m.isEditing {
-				clipboard.WriteAll(m.task.GetText())
-			}
-
-		case tea.KeyCtrlS:
-			if m.isEditing {
-				if vtEditor, ok := m.editor.(vimtea.Editor); ok {
-					m.appContent = vtEditor.GetBuffer().Text()
-				} else {
-					log.Println("Warning: Editor is not a vimtea.editor, cannot get buffer")
-				}
-
-				renderedContent, err := glamour.Render(m.appContent, "dark")
-				if err != nil {
-					return m, nil
-				}
-
-				m.viewport.SetContent(renderedContent)
-				m.textarea.Focus()
-
-				m.isEditing = false
-				return m, tea.Batch(tiCmd, vpCmd)
-			}
-
-		case tea.KeyCtrlE:
-			if !m.isEditing {
-				m.textarea.Blur()
-
-				m.isEditing = true
-
-				m.editor = vimtea.NewEditor(
-					vimtea.WithContent(m.appContent),
-					vimtea.WithFileName("task.txt"),
-					vimtea.WithFullScreen(),
-				)
-
-				m.editor, editorCmd = m.editor.Update(tea.WindowSizeMsg{Width: m.width, Height: m.height})
-
-				cmds = append(cmds, editorCmd)
-				cmds = append(cmds, m.editor.Init())
-
-				return m, tea.Batch(cmds...)
-			}
+		def := keyMap[msg.Type]
+		if def != nil {
+			return def()
 		}
 
 	case generationMsg:
@@ -132,10 +73,10 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case spinner.TickMsg:
 		if m.isLoading {
-			m.spinner, spCmd = m.spinner.Update(msg)
-			cmds = append(cmds, spCmd)
+			m.spinner, b.spCmd = m.spinner.Update(msg)
+			b.cmds = append(b.cmds, b.spCmd)
 
-			return m, tea.Batch(cmds...)
+			return m, tea.Batch(b.cmds...)
 		}
 
 	case tea.WindowSizeMsg:
@@ -147,15 +88,15 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.textarea.SetWidth(msg.Width)
 
 		if m.isEditing {
-			m.editor, editorCmd = m.editor.Update(msg)
-			cmds = append(cmds, editorCmd)
+			m.editor, b.editorCmd = m.editor.Update(msg)
+			b.cmds = append(b.cmds, b.editorCmd)
 		}
 
-		m.textarea, tiCmd = m.textarea.Update(msg)
-		m.viewport, vpCmd = m.viewport.Update(msg)
+		m.textarea, b.tiCmd = m.textarea.Update(msg)
+		m.viewport, b.vpCmd = m.viewport.Update(msg)
 
-		cmds = append(cmds, tiCmd, vpCmd)
+		b.cmds = append(b.cmds, b.tiCmd, b.vpCmd)
 	}
 
-	return m, tea.Batch(cmds...)
+	return m, tea.Batch(b.cmds...)
 }
